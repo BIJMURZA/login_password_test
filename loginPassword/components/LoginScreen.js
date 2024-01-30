f86a9576d42da33696c046aa64e0f116013d88dd
KUROSAKI <xxxbijumrzaxxx@gmail.com>
2024-01-30 11:48:34 +0700

    Авторизация с помощью Post запроса
    Если логин и пароль верны, то откроется страница с HomeScreen

--- loginPassword/components/LoginScreen.js
+++ loginPassword/components/LoginScreen.js
@@ -1,30 +1,42 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { View, Button, TextInput, StyleSheet } from 'react-native';
+import { View, Button, TextInput, StyleSheet, Text } from 'react-native';
+
 
 
 const LoginScreen = ({ onSuccess }) => {
-    const login = "hatiko_1945";
-    const password = "12345";
-
     const [loginInput, setLogin] = useState('');
     const [passwordInput, setPassword] = useState(' ')
 
     const authorization = () => {
-        if (loginInput === login && passwordInput === password) {
-            onSuccess();
-        }
-        else {
-            alert('Неверный логин или пароль!')
-        }
+        fetch(`http://192.168.0.117:3000/accounts/login/password`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                login: loginInput,
+                password: passwordInput
+            }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    onSuccess();
+                } else {
+                    alert('Неверный логин или пароль! \nПовторите попытку');
+                }
+            })
+            .catch(err => {console.error('!Авторизация ERROR!', err);})
     }
 
     return (
         <>
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <TextInput style={styles.input} onChangeText={setLogin} value={loginInput} placeholder="Логин ..."/>
-                <TextInput secureTextEntry={true} style={styles.input} onChangeText={setPassword} value={passwordInput} placeholder="Пароль ..."/>
+                <TextInput secureTextEntry={true} style={styles.input} onChangeText={setPassword} value={passwordInput}
+                           placeholder="Пароль..."/>
                 <Button title="Авторизация" color="blue" onPress={authorization}/>
             </View>
         </>
